1775998f67c2c4d6a76187d5e5a0075ce1141f5f
Will Christensen <will.christensen@defenseunicorns.com>
2023-07-03 14:38:45 -0400

    switch to bastion id and add aws ssm ssh config

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -3,9 +3,12 @@
 info_file=test/bastion_info.txt
 
 eks_name=$1
+bastion_id=$(grep -e "bastion_id" $info_file|awk -F "=" '{print $2}')
 bastion_dns=$(grep -e "bastion_dns" $info_file|awk -F "=" '{print $2}')
 bastion_cidr=$(grep -e "bastion_cidr" $info_file|awk -F "=" '{print $2}')
 bastion_region=$(grep -e "bastion_region" $info_file|awk -F "=" '{print $2}')
 bastion_pass=$(grep -e "bastion_pass" $info_file|awk -F "=" '{print $2}')
 
-sshuttle --dns -vr ec2-user@$bastion_dns $bastion_cidr --ssh-cmd 'ssh -p "${bastion_pass}"'
+# sshuttle --dns -vr ec2-user@$bastion_dns $bastion_cidr --ssh-cmd 'ssh -p "${bastion_pass}"'
+
+sshuttle -D -e '"'"'sshpass -p "${bastion_pass}" ssh -q -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="aws ssm --region ${bastion_region} start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"'"'"' --dns --disable-ipv6 -vr ec2-user@${bastion_id} ${bastion_cidr} 
