b8a73f684598caf3b9b31e544141f83aa72750ad
Will Christensen <will.christensen@defenseunicorns.com>
2023-07-02 22:22:12 -0400

    test of reverting policy creation

--- main.tf
+++ main.tf
@@ -70,7 +70,22 @@ resource "aws_iam_role" "auth_eks_role" {
   name                 = "${var.name}-auth-eks-role"
   description          = "EKS AuthConfig Role"
   permissions_boundary = var.iam_role_permissions_boundary
-  assume_role_policy   = local.auth_eks_role_policy
+  # assume_role_policy   = local.auth_eks_role_policy
+  assume_role_policy   = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+              "AWS": ${length(local.admin_arns) == 0 ? "[]" : jsonencode(local.admin_arns)}
+            },
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+}
+EOF
 }
 
 #---------------------------------------------------------------
