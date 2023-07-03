cabff5ed4fe57d7f5fa233ac2279d6d7442d6a0e
Will Christensen <will.christensen@defenseunicorns.com>
2023-07-03 15:18:28 -0400

    small fixes bastion connect

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Script to run from a test setup to connect to the bastion and allow Terraform and helm to setup the cluster
 
 info_file=test/bastion_info.txt
 
@@ -11,4 +12,8 @@ bastion_pass=$(grep -e "bastion_pass" $info_file|awk -F "=" '{print $2}')
 
 # sshuttle --dns -vr ec2-user@$bastion_dns $bastion_cidr --ssh-cmd 'ssh -p "${bastion_pass}"'
 
-sshuttle -D -e '"'"'sshpass -p "${bastion_pass}" ssh -q -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="aws ssm --region ${bastion_region} start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"'"'"' --dns --disable-ipv6 -vr ec2-user@${bastion_id} ${bastion_cidr} 
+sshuttle -D -e '"'"'sshpass -p "${bastion_pass}" ssh -q -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ProxyCommand="aws ssm --region ${bastion_region} start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"'"'"' --dns --disable-ipv6 -vr ec2-user@${bastion_id} ${bastion_cidr} &
+
+sleep 5
+
+aws eks update-kubeconfig --name $eks_name --region $bastion_region
