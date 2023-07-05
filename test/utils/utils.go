e091a304bf4b6bd3a0d08f94c16d89488ff4730b
Will Christensen <will.christensen@defenseunicorns.com>
2023-07-05 14:10:51 -0400

    allow multiple kubeconfigs to be used in parallel
    Co-authored-by: Andy Mills <CloudBeard@users.noreply.github.com>
    Co-authored-by: Palassis <MxNxPx@users.noreply.github.com>
    Co-authored-by: Micah Nagel <mjnagel@users.noreply.github.com>

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -280,10 +280,10 @@ func ConfigureKubeconfig(t *testing.T, tempFolder string) {
 	}
 	eksClusterName := terraform.Output(t, terraformOutputOptions, "eks_cluster_name")
 	region := terraform.Output(t, terraformOutputOptions, "bastion_region")
-	err := exec.Command("bash", "-c", fmt.Sprintf("mkdir -p ~/.kube && aws eks update-kubeconfig --name %s --alias %s --region %s", eksClusterName, eksClusterName, region)).Run() //nolint:gosec
+	err := exec.Command("bash", "-c", fmt.Sprintf("mkdir -p ~/.kube && aws eks update-kubeconfig --name %s --alias %s --region %s --kubeconfig ~/.kube/%s", eksClusterName, eksClusterName, region, eksClusterName)).Run() //nolint:gosec
 	require.NoError(t, err)
 	// Make sure it worked. This command should return without error
-	err = exec.Command("bash", "-c", "kubectl get nodes").Run()
+	err = exec.Command("bash", "-c", "kubectl get nodes --kubeconfig ~/.kube/%s", eksClusterName).Run()
 	require.NoError(t, err)
 }
 
