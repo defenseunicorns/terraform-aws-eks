6a5dff0cb4b237325641cf559c8bcbf2b0c1fe02
Gabe Scarberry <ntwkninja@gmail.com>
2023-07-24 09:09:05 -0500

    example updates / test k8s 1.27

--- variables.tf
+++ variables.tf
@@ -9,9 +9,9 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version to use for EKS cluster"
   type        = string
-  default     = "1.26"
+  default     = "1.27"
   validation {
-    condition     = contains(["1.26"], var.cluster_version)
+    condition     = contains(["1.26","1.27"], var.cluster_version)
     error_message = "Kubernetes version must be equal to one that we support. See EKS module variables for supported versions."
   }
 }
