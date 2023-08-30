6b1d1f33b01715c7f88cbec54ce96432761eb6f0
Zack Annexstein <zannexstein@gmail.com>
2023-08-30 13:02:41 -0700

    use utils

--- test/e2e/examples_complete_plan_only_test.go
+++ test/e2e/examples_complete_plan_only_test.go
@@ -3,12 +3,17 @@ package e2e_test
 import (
 	"testing"
 
+	"github.com/defenseunicorns/delivery_aws_iac_utils/pkg/utils"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	teststructure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 func TestExamplesCompletePlanOnly(t *testing.T) {
 	t.Parallel()
+
+	// Set the TF_VAR_region to us-east-2 if it's not already set
+	utils.SetDefaultEnvVar("TF_VAR_region", "us-east-2")
+
 	tempFolder := teststructure.CopyTerraformFolderToTemp(t, "../..", "examples/complete")
 	terraformOptionsPlan := &terraform.Options{
 		TerraformDir: tempFolder,
