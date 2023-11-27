09c608c695923f0df3ea9958f3d9f4e919a17e6f
Zack Annexstein <zannexstein@gmail.com>
2023-11-27 13:19:03 -0800

    bump the ref

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -108,7 +108,7 @@ data "aws_ami" "amazonlinux2" {
 }
 
 module "bastion" {
-  source = "git::https://github.com/defenseunicorns/terraform-aws-bastion.git?ref=tags/v0.0.1-alpha"
+  source = "git::https://github.com/defenseunicorns/terraform-aws-bastion.git?ref=v0.1.4"
 
   count = var.enable_bastion ? 1 : 0
 
